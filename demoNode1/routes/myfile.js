d88d2d8cc6e198670e28d9806d13b2de932833a6
tauqeer.shakir@yahoo.com <tauqeer.shakir@yahoo.com>
2013-09-09 08:53:43 -0700

    Changes

---
+++ demoNode1/routes/myfile.js
@@ -0,0 +1,11 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: Tauqeer ahmed
+ * Date: 9/9/13
+ * Time: 8:32 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+exports.myfunction=function(req,res){
+    res.send('Hello this is my own file');
+}
